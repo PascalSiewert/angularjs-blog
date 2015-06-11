eba566781e2dd85b2e8453bd5c189c589ef7f80c
Pascal Siewert <pascalsiewert@googlemail.com>
2015-06-11 15:47:25 +0200

    added simple routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('BlogApp', []);
+var app = angular.module('BlogApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'PostController',
+      templateUrl: 'views/posts.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
