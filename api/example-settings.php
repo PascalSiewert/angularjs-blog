68e0dd3adc8d93c2ee0f3bad8babfb0b6bc2083d
Pascal Siewert <pascalsiewert@googlemail.com>
2015-06-19 22:49:23 +0200

    added example-settings to the project

---
+++ api/example-settings.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'YOUR_HOST');
+define('DB_NAME', 'YOUR_DATABASE_NAME');
+define('DB_USER', 'YOUR_DATABASE_USERNAME');
+define('DB_PASSWORD', 'YOUR_DATABASE_PASSWORD');
